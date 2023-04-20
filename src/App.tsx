6e681f2d4889afe52af4b6b091a5b31c191c976f
huytu20 <danghuytu.thd2006@gmail.com>
2023-04-20 22:53:46 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,211 @@
+
+
+const App = () => {
+  return (
+    <div>
+      <div className="d-flex flex-column flex-root" id="kt_app_root">
+			
+			{/* <style>body { background-image: url('assets/media/auth/bg10.jpeg'); } [data-bs-theme="dark"] body { background-image: url('assets/media/auth/bg10-dark.jpeg'); }</style> */}
+			
+			
+			<div className="d-flex flex-column flex-lg-row flex-column-fluid">
+				
+				<div className="d-flex flex-lg-row-fluid">
+					
+					<div className="d-flex flex-column flex-center pb-0 pb-lg-10 p-10 w-100">
+						
+						<img className="theme-light-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="assets/media/auth/agency.png" alt="" />
+						<img className="theme-dark-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="assets/media/auth/agency-dark.png" alt="" />
+						
+						
+						<h1 className="text-gray-800 fs-2qx fw-bold text-center mb-7">Fast, Efficient and Productive</h1>
+						
+						
+						<div className="text-gray-600 fs-base text-center fw-semibold">In this kind of post,
+						<a href="#" className="opacity-75-hover text-primary me-1">the blogger</a>introduces a person they’ve interviewed
+						<br />and provides some background information about
+						<a href="#" className="opacity-75-hover text-primary me-1">the interviewee</a>and their
+						<br />work following this is a transcript of the interview.</div>
+						
+					</div>
+					
+				</div>
+				
+				
+				<div className="d-flex flex-column-fluid flex-lg-row-auto justify-content-center justify-content-lg-end p-12">
+					
+					<div className="bg-body d-flex flex-column flex-center rounded-4 w-md-600px p-10">
+						
+						<div className="d-flex flex-center flex-column align-items-stretch h-lg-100 w-md-400px">
+							
+							<div className="d-flex flex-center flex-column flex-column-fluid pb-15 pb-lg-20">
+								
+								<form className="form w-100" id="kt_sign_in_form" data-kt-redirect-url="../../demo39/dist/index.html" action="#">
+									
+									<div className="text-center mb-11">
+										
+										<h1 className="text-dark fw-bolder mb-3">Sign In</h1>
+										
+										
+										<div className="text-gray-500 fw-semibold fs-6">Your Social Campaigns</div>
+										
+									</div>
+									
+									
+									<div className="row g-3 mb-9">
+										
+										<div className="col-md-6">
+											
+											<a href="#" className="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100">
+											<img alt="Logo" src="assets/media/svg/brand-logos/google-icon.svg" className="h-15px me-3" />Sign in with Google</a>
+											
+										</div>
+										
+										
+										<div className="col-md-6">
+											
+											<a href="#" className="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100">
+											<img alt="Logo" src="assets/media/svg/brand-logos/apple-black.svg" className="theme-light-show h-15px me-3" />
+											<img alt="Logo" src="assets/media/svg/brand-logos/apple-black-dark.svg" className="theme-dark-show h-15px me-3" />Sign in with Apple</a>
+											
+										</div>
+										
+									</div>
+									
+									
+									<div className="separator separator-content my-14">
+										<span className="w-125px text-gray-500 fw-semibold fs-7">Or with email</span>
+									</div>
+									
+									
+									<div className="fv-row mb-8">
+										
+										<input type="text" placeholder="Email" name="email"  className="form-control bg-transparent" />
+										
+									</div>
+									
+									<div className="fv-row mb-3">
+										
+										<input type="password" placeholder="Password" name="password" className="form-control bg-transparent" />
+										
+									</div>
+									
+									
+									<div className="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
+										<div></div>
+										
+										<a href="../../demo39/dist/authentication/layouts/overlay/reset-password.html" className="link-primary">Forgot Password ?</a>
+										
+									</div>
+									
+									
+									<div className="d-grid mb-10">
+										<button type="submit" id="kt_sign_in_submit" className="btn btn-primary">
+											
+											<span className="indicator-label">Sign In</span>
+											
+											
+											<span className="indicator-progress">Please wait...
+											<span className="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+											
+										</button>
+									</div>
+									
+									
+									<div className="text-gray-500 text-center fw-semibold fs-6">Not a Member yet?
+									<a href="../../demo39/dist/authentication/layouts/overlay/sign-up.html" className="link-primary">Sign up</a></div>
+									
+								</form>
+								
+							</div>
+							
+							
+							<div className="d-flex flex-stack">
+								
+								<div className="me-10">
+									
+									<button className="btn btn-flex btn-link btn-color-gray-700 btn-active-color-primary rotate fs-base" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-start" data-kt-menu-offset="0px, 0px">
+										<img data-kt-element="current-lang-flag" className="w-20px h-20px rounded me-3" src="assets/media/flags/united-states.svg" alt="" />
+										<span data-kt-element="current-lang-name" className="me-1">English</span>
+										<i className="ki-outline ki-down fs-5 text-muted rotate-180 m-0"></i>
+									</button>
+									
+									
+									<div className="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg-light-primary fw-semibold w-200px py-4 fs-7" data-kt-menu="true" id="kt_auth_lang_menu">
+										
+										<div className="menu-item px-3">
+											<a href="#" className="menu-link d-flex px-5" data-kt-lang="English">
+												<span className="symbol symbol-20px me-4">
+													<img data-kt-element="lang-flag" className="rounded-1" src="assets/media/flags/united-states.svg" alt="" />
+												</span>
+												<span data-kt-element="lang-name">English</span>
+											</a>
+										</div>
+										
+										
+										<div className="menu-item px-3">
+											<a href="#" className="menu-link d-flex px-5" data-kt-lang="Spanish">
+												<span className="symbol symbol-20px me-4">
+													<img data-kt-element="lang-flag" className="rounded-1" src="assets/media/flags/spain.svg" alt="" />
+												</span>
+												<span data-kt-element="lang-name">Spanish</span>
+											</a>
+										</div>
+										
+										
+										<div className="menu-item px-3">
+											<a href="#" className="menu-link d-flex px-5" data-kt-lang="German">
+												<span className="symbol symbol-20px me-4">
+													<img data-kt-element="lang-flag" className="rounded-1" src="assets/media/flags/germany.svg" alt="" />
+												</span>
+												<span data-kt-element="lang-name">German</span>
+											</a>
+										</div>
+										
+										
+										<div className="menu-item px-3">
+											<a href="#" className="menu-link d-flex px-5" data-kt-lang="Japanese">
+												<span className="symbol symbol-20px me-4">
+													<img data-kt-element="lang-flag" className="rounded-1" src="assets/media/flags/japan.svg" alt="" />
+												</span>
+												<span data-kt-element="lang-name">Japanese</span>
+											</a>
+										</div>
+										
+										
+										<div className="menu-item px-3">
+											<a href="#" className="menu-link d-flex px-5" data-kt-lang="French">
+												<span className="symbol symbol-20px me-4">
+													<img data-kt-element="lang-flag" className="rounded-1" src="assets/media/flags/france.svg" alt="" />
+												</span>
+												<span data-kt-element="lang-name">French</span>
+											</a>
+										</div>
+										
+									</div>
+									
+								</div>
+								
+								
+								<div className="d-flex fw-semibold text-primary fs-base gap-5">
+									<a href="../../demo39/dist/pages/team.html" target="_blank">Terms</a>
+									<a href="../../demo39/dist/pages/pricing/column.html" target="_blank">Plans</a>
+									<a href="../../demo39/dist/pages/contact.html" target="_blank">Contact Us</a>
+								</div>
+								
+							</div>
+							
+						</div>
+						
+					</div>
+					
+				</div>
+				
+			</div>
+			
+		</div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/index.tsx
@@ -0,0 +1,28 @@
+// Apps
+import './_metronic/assets/fonticon/fonticon.css'
+import './_metronic/assets/keenicons/duotone/style.css'
+import './_metronic/assets/keenicons/outline/style.css'
+import './_metronic/assets/keenicons/solid/style.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+/**
+ * TIP: Replace this style import with rtl styles to enable rtl mode
+ *
+ * import './_metronic/assets/css/style.rtl.css'
+ **/
+import './_metronic/assets/sass/style.scss'
+import './_metronic/assets/sass/plugins.scss'
+import './_metronic/assets/sass/style.react.scss'
+import  App  from './App'
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
